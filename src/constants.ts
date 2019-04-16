036bbbb3f73ded5f64f9cc880b01cf70a2680d81
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-16 09:26:06 -0700

    1.0.13: generic nominal constants preclude need for insteadOf

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,14 @@
 // tslint:disable no-magic-numbers
 
-import { Hz, insteadOf, Meters, Ms, NO_TRANSLATION, of, Scalar, to, Translation } from '@musical-patterns/utilities'
+import { Hz, Meters, Ms, NO_TRANSLATION, of, Scalar, to, Translation } from '@musical-patterns/utilities'
 
 const STANDARD_BASE_FREQUENCY: Scalar<Hz> = to.Scalar(of.Hz(700))
 const STANDARD_BASE_DURATION: Scalar<Ms> = to.Scalar(of.Ms(700))
-const STANDARD_BASE_POSITION: Array<Translation<Meters>> =
-    [ NO_TRANSLATION, NO_TRANSLATION, NO_TRANSLATION ]
-    // tslint:disable-next-line no-unnecessary-callback-wrapper
-        .map((translation: Translation) => insteadOf<Translation, Meters>(translation))
+const STANDARD_BASE_POSITION: Array<Translation<Meters>> = [ NO_TRANSLATION, NO_TRANSLATION, NO_TRANSLATION ]
+
 const STANDARD_BASE_POSITION_SCALAR: Scalar<Meters> = to.Scalar(of.Meters(1))
-const STANDARD_BASE_DURATION_TRANSLATION: Translation<Ms> = insteadOf<Translation, Ms>(NO_TRANSLATION)
-const STANDARD_BASE_FREQUENCY_TRANSLATION: Translation<Hz> = insteadOf<Translation, Hz>(NO_TRANSLATION)
+const STANDARD_BASE_DURATION_TRANSLATION: Translation<Ms> = NO_TRANSLATION
+const STANDARD_BASE_FREQUENCY_TRANSLATION: Translation<Hz> = NO_TRANSLATION
 
 export {
     STANDARD_BASE_POSITION,
