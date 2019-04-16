0f9e562c2a9ca5416f1501f5e6939bf21de44b0b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-16 08:25:31 -0700

    1.0.12: improve insteadOf

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,10 @@ const STANDARD_BASE_DURATION: Scalar<Ms> = to.Scalar(of.Ms(700))
 const STANDARD_BASE_POSITION: Array<Translation<Meters>> =
     [ NO_TRANSLATION, NO_TRANSLATION, NO_TRANSLATION ]
     // tslint:disable-next-line no-unnecessary-callback-wrapper
-        .map((translation: Translation) => insteadOf<Meters, number, 'Translation'>(translation))
+        .map((translation: Translation) => insteadOf<Translation, Meters>(translation))
 const STANDARD_BASE_POSITION_SCALAR: Scalar<Meters> = to.Scalar(of.Meters(1))
-const STANDARD_BASE_DURATION_TRANSLATION: Translation<Ms> = insteadOf<Ms, number, 'Translation'>(NO_TRANSLATION)
-const STANDARD_BASE_FREQUENCY_TRANSLATION: Translation<Hz> = insteadOf<Hz, number, 'Translation'>(NO_TRANSLATION)
+const STANDARD_BASE_DURATION_TRANSLATION: Translation<Ms> = insteadOf<Translation, Ms>(NO_TRANSLATION)
+const STANDARD_BASE_FREQUENCY_TRANSLATION: Translation<Hz> = insteadOf<Translation, Hz>(NO_TRANSLATION)
 
 export {
     STANDARD_BASE_POSITION,
