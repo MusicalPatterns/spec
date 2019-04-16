2373ea6b81c9c7b5c765f58a801b089eff389229
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-16 10:59:01 -0700

    1.0.14: use generic parameter of to where possible

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 // tslint:disable no-magic-numbers
 
-import { Hz, Meters, Ms, NO_TRANSLATION, of, Scalar, to, Translation } from '@musical-patterns/utilities'
+import { Hz, Meters, Ms, NO_TRANSLATION, Scalar, to, Translation } from '@musical-patterns/utilities'
 
-const STANDARD_BASE_FREQUENCY: Scalar<Hz> = to.Scalar(of.Hz(700))
-const STANDARD_BASE_DURATION: Scalar<Ms> = to.Scalar(of.Ms(700))
+const STANDARD_BASE_FREQUENCY: Scalar<Hz> = to.Scalar<Hz>(700)
+const STANDARD_BASE_DURATION: Scalar<Ms> = to.Scalar<Ms>(700)
 const STANDARD_BASE_POSITION: Array<Translation<Meters>> = [ NO_TRANSLATION, NO_TRANSLATION, NO_TRANSLATION ]
 
-const STANDARD_BASE_POSITION_SCALAR: Scalar<Meters> = to.Scalar(of.Meters(1))
+const STANDARD_BASE_POSITION_SCALAR: Scalar<Meters> = to.Scalar<Meters>(1)
 const STANDARD_BASE_DURATION_TRANSLATION: Translation<Ms> = NO_TRANSLATION
 const STANDARD_BASE_FREQUENCY_TRANSLATION: Translation<Hz> = NO_TRANSLATION
 
