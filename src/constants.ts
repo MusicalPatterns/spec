c2e998959febbe826f05817cbaf1db5820240a08
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-29 14:19:36 -0700

    1.0.30: musicalAs

--- src/constants.ts
+++ src/constants.ts
@@ -1,23 +1,11 @@
 // tslint:disable no-magic-numbers
 
-import {
-    as,
-    Duration,
-    Hz,
-    Meters,
-    Ms,
-    NO_SHIFT,
-    Pitch,
-    Point,
-    Position,
-    Scalar,
-    Translation,
-} from '@musical-patterns/utilities'
+import { as, Duration, musicalAs, NO_SHIFT, Pitch, Position, Scalar, Translation } from '@musical-patterns/utilities'
 
-const STANDARD_BASIS_FREQUENCY: Pitch = as.Point<Hz>(700)
-const STANDARD_BASIS_DURATION: Duration = as.Delta<Ms>(700)
+const STANDARD_BASIS_FREQUENCY: Pitch = musicalAs.Pitch(700)
+const STANDARD_BASIS_DURATION: Duration = musicalAs.Duration(700)
 const STANDARD_BASIS_POSITION: Position[] =
-    [ 0, 0, 0 ].map((dimension: number) => as.Point<Meters>(dimension))
+    [ 0, 0, 0 ].map((dimension: number) => musicalAs.Position(dimension))
 
 const STANDARD_BASIS_POSITION_SCALAR: Scalar<Position> = as.Scalar<Position>(1)
 const STANDARD_BASIS_DURATION_TRANSLATION: Translation<Duration> = NO_SHIFT
